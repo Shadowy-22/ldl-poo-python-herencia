53179b75faeb7325e324c015bbb64c69a27bdc00
Juan Bautista Aguilar <juanbautistaaguilar21@gmail.com>
2026-04-26 19:51:25 -0300

    Removido los type suggestions

--- src/modules/jugador.py
+++ src/modules/jugador.py
@@ -1,9 +1,9 @@
 ## Clase mas general
 class Jugador:
-    def __init__(self, numeroCamiseta: int, apellido: str, minutosJugados: int):
-        self.numeroCamiseta: int = numeroCamiseta
-        self.apellido: str = apellido
-        self.minutosJugados: int = minutosJugados
+    def __init__(self, numeroCamiseta, apellido, minutosJugados):
+        self.numeroCamiseta = numeroCamiseta
+        self.apellido = apellido
+        self.minutosJugados = minutosJugados
 
     def posicion(self):
         return self.__class__.__name__
