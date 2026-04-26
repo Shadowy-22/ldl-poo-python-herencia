20c6cf2cc94e00a6df866cda7f8adbd905b7ad33
Juan Bautista Aguilar <119075581+Shadowy-22@users.noreply.github.com>
2026-04-26 19:44:26 -0300

    Remove constructor from Arquero class
    Removed the constructor from the Arquero class.

--- src/modules/arquero.py
+++ src/modules/arquero.py
@@ -1,7 +1,5 @@
 from .jugador import Jugador
 
 class Arquero(Jugador):
-    def __init__(self, numeroCamiseta, apellido, minutosJugados):
-        super().__init__(numeroCamiseta, apellido, minutosJugados)
-
+   pass
 
