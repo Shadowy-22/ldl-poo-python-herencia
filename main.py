de5a17ef1e2bb1ea0847eb720b19a10e6d414321
ocscafini21 <ocscafini@alumnos.unsada.edu.ar>
2026-04-27 18:39:15 -0300

    Pruebas de funcionalidad y estrés completadas

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from src.ui.menu import * 
+from src.ui.menu import iniciar_menu
 
-## Entry Point de la App
-## TODO: Implementar main
 def main():
-   ...
+    # Solo llamamos a la función de Leo
+    iniciar_menu()
 
-
-## Aseguramos que main solo se ejecute cuando el script es ejecutado directamente 
-## Si importamos este archivo desde otro lado, NO se ejecuta automáticamente 
 if __name__ == "__main__":
-        main()
+    main()
